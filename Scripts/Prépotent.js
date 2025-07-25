c1400ca53abbe2ee61dc576b9c1cfd7985c2bd38
Lyppo <74815001+Lyppo@users.noreply.github.com>
2025-07-25 17:53:19 +0200

    responsive pour téléphone pris en charge, bouton retour fonctionnel, et radial gradiant desactivé pour le moment

--- Scripts/Prépotent.js
+++ Scripts/Prépotent.js
@@ -15,12 +15,40 @@ function aime_pas() {
     next();
 }
 
+/* reinplémenter radial gradiant mais retravaillé pour téléphone */
+
+async function retour() {
+
+    element -= 1;
+
+    if (element < 0) {
+        if (page > 0) {
+            page-=1;
+            element = 19;
+        }
+
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: "Bearer " + MON_TOKEN
+            }
+        };
+
+        const reponse = await fetch("https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=" + page, options);
+        data = await reponse.json();
+    }
+
+    document.getElementById("affiche").src = "https://image.tmdb.org/t/p/original" + await data.results[element].poster_path;
+
+    document.getElementById("arriere-plan-poster").style.backgroundImage = "url(https://image.tmdb.org/t/p/original" + await data.results[element].backdrop_path + ")";
+
+    document.getElementById("bouton-vus").textContent = page + " - " + element;
+}
+
 async function next() {
 
-    console.log("Film suivant...");
-
     element += 1;
-
     if (element > 19) {
         page+=1;
         element = 0;
@@ -37,7 +65,8 @@ async function next() {
         data = await reponse.json();
     }
 
-    document.getElementById("affiche").src = "https://image.tmdb.org/t/p/original" + data.results[element].poster_path;
+    document.getElementById("affiche").src = "https://image.tmdb.org/t/p/original" + await data.results[element].poster_path;
 
-    document.getElementById("arriere-plan-poster").style.backgroundImage = "radial-gradient(circle at center, #000a, #000f), url(https://image.tmdb.org/t/p/original" + data.results[element].backdrop_path + ")";
+    document.getElementById("arriere-plan-poster").style.backgroundImage = "url(https://image.tmdb.org/t/p/original" + await data.results[element].backdrop_path + ")";
+    document.getElementById("bouton-vus").textContent = page + " - " + element;
 }
